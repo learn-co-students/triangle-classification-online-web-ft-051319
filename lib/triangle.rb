67a61c7d283d230295e87286d501446c43b98687
github username <github email address>
2019-05-30 13:08:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+# lib/triangle.rb
+
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    sides = [one, two, three]
+    if sides.any? {|side| side <= 0} == true || (one + two <= three || one + three <= two || two + three <= one)
+    raise TriangleError
+    elsif
+      one == two && two == three
+      :equilateral
+    elsif
+      one == two || one == three || two == three
+      :isosceles
+    else
+      !(one == two || one == three || two == three)
+      :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
